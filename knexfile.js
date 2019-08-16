212df02c4dc48b1a1a67e77b69d65d2e49e6a010
David Engel <Davidjengel2012@gmail.com>
2019-08-16 09:39:38 -0600

    Add configuration for heroku

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,11 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 
 };
