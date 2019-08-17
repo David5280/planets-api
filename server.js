b9ff23bb03117e09e5aa39b33cb4f98c6b1dd13a
David Engel <Davidjengel2012@gmail.com>
2019-08-17 11:06:09 -0600

    Connect planets table to server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
+const dbConnection = require('./db/seeds/dev/connection')
 const environment = process.env.NODE_ENV || 'development';
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Planets API';
 
 app.get('/', (request, response) => {
-  response.send('Planets server is running.');
+  response.send('Planets server is running');
 });
+
+app.get('/api/v1/planets', (req, res) => {
+  dbConnection('planets')
+    .select('*')
+    .then(planets => res.json(planets))
+    .catch(error => res.json({ error: error.message, stack: error.stack }))
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
+  
