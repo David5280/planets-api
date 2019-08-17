640c9628e7fad87b9984544c175bfe0d3b07bd26
David Engel <Davidjengel2012@gmail.com>
2019-08-17 11:07:17 -0600

    Connect moons table to server.js

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ app.get('/api/v1/planets', (req, res) => {
     .select('*')
     .then(planets => res.json(planets))
     .catch(error => res.json({ error: error.message, stack: error.stack }))
-})
+});
+
+app.get('/api/v1/moons', (req, res) => {
+  dbConnection('moons')
+    .select('*')
+    .then(moons => res.json(moons))
+    .catch(error => res.json({ error: error.message, stack: error.stack }))
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
